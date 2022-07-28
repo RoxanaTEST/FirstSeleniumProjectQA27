0bad164d5dde7b3bdd5d0ffc9e0136192818a498
Roxana Pop <mihaela.roxana.pop@gmail.com>
2022-07-28 17:56:15 +0300

    my commit

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -11,8 +11,8 @@ public class LoginTest {
         driver.get("http://testfasttrackit.info/selenium-test");
         driver.findElement(By.cssSelector("#header > div > div.skip-links > div > a > span.label")).click();
         driver.findElement(By.cssSelector("#header-account > div > ul > li.last > a")).click();
-        driver.findElement(By.id("email")).sendKeys("cosmin@fasttrackit.org");
-        driver.findElement(By.id("pass")).sendKeys("123456");
+        driver.findElement(By.id("email")).sendKeys("roxanatestare@gmail.com");
+        driver.findElement(By.id("pass")).sendKeys("Testareplatforma!");
         driver.findElement(By.id("send2")).click();
         driver.quit();
     }
